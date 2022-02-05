72e4b3e1c186048c1ec6a62563b493b17b008f22
sylvia <charles.w.developer@gmail.com>
2022-02-05 12:24:12 -0800

    Changed naming convention

--- app/src/pages/arena.vue
+++ app/src/pages/arena.vue
@@ -1,26 +1,14 @@
 <template>
     <div class="flex w-full justify-center">
         <div class="flex flex-col mt-16 w-4/5">
-            <div
-                class="font-display uppercase font-bold text-5xl text-gray-500"
-            >
-                Arena
-            </div>
+            <div class="font-display uppercase font-bold text-5xl text-gray-500">Arena</div>
 
-            <div
-                class="
-                    flex
-                    w-full
-                    h-full
-                    p-4
-                    border-2 border-gray-300
-                    rounded-lg
-                "
-            >
+            <div class="flex w-full h-full p-4 border-2 border-gray-300 rounded-lg">
                 <!-- left container -->
                 <div class="rounded-lg w-1/3 flex flex-col">
                     <card
                         v-for="(item, index) in cards"
+                        :key="index"
                         :name="item.name"
                         :classname="item.classname"
                     />
@@ -30,6 +18,7 @@
                 <div class="rounded-lg w-2/3 flex flex-col">
                     <card
                         v-for="(item, index) in cards"
+                        :key="index"
                         :name="item.name"
                         :classname="item.classname"
                     />
@@ -41,7 +30,7 @@
 
 <script>
 import { reactive } from 'vue'
-import card from '../components/card.vue'
+import card from '../components/atoms/card.vue'
 
 export default {
     components: {
