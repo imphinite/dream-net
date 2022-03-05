052934afd16883c2d8f5c4023241a85484ff10b7
sylvia <charles.w.developer@gmail.com>
2022-03-05 12:53:42 -0800

    Date picker initial setup

--- app/src/components/atoms/date-picker.vue
+++ app/src/components/atoms/date-picker.vue
@@ -1,8 +1,14 @@
-<template></template>
+<template>
+    <date-picker />
+</template>
 
 <script>
+import DatePicker from 'vue3-date-time-picker'
+import 'vue3-date-time-picker/dist/main.css'
+
 export default {
     name: 'dn-date-picker',
+    components: { DatePicker },
     setup() {},
 }
 </script>
