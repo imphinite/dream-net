d0582f76efb4757748f8ed6344fe7bc9e57462d7
Charles Wang <charles.w.developer@gmail.com>
2022-04-22 21:02:59 -0700

    Init pages

--- app/src/pages/Home.vue
+++ app/src/pages/Home.vue
@@ -1,111 +1,70 @@
 <template>
-    <article :class="containerStyles">
-        <dn-header @menu-button-click="navDrawer = !navDrawer" />
-
-        <section class="scrollbar-hidden" :class="bodySectionStyles">
-            <slot>
-                <dn-card
-                    v-for="(post, index) in homeFeed.posts"
-                    :key="index"
-                    class="snap-start mt-2"
-                    :title="post.title"
-                    :content="getContentFromPostBody(post.body)"
-                />
-            </slot>
-        </section>
-    </article>
+    <dn-page
+        @toggle-navigation-drawer="navDrawer = !navDrawer"
+        @reload="$router.go()"
+    >
+        <dn-card
+            v-for="(post, index) in homeFeed.posts"
+            :key="index"
+            class="snap-start mt-2"
+            :title="post.title"
+            :content="getContentFromPostBody(post.body)"
+            @title-click="goToPost(post)"
+        />
+    </dn-page>
 </template>
-
-<style scoped>
-.scrollbar-hidden::-webkit-scrollbar {
-    width: 0 !important;
-}
-.scrollbar-hidden {
-    overflow: -moz-scrollbars-none;
-    -ms-overflow-style: none;
-}
-</style>
 
 <script>
 //-- Libraries
-import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 
 //-- Components
 import DnHeader from '@cm/header.vue'
 import DnNavigationDrawer from '@co/navigation-drawer.vue'
 import DnCard from '@cm/card.vue'
+import DnPage from '@ct/page.vue'
 
 //-- Store
 import useStore from '@/store/use-store'
 
 //-- Composables
 import useNavigationDrawer from '@/composables/use-navigation-drawer'
-import useGradients from '@/composables/use-gradients'
 
 export default {
     name: 'dn-home',
-    components: { DnHeader, DnNavigationDrawer, DnCard },
-    data() {
-        return {
-            showNavigationDrawer: false,
-        }
-    },
+    components: { DnHeader, DnNavigationDrawer, DnCard, DnPage },
     setup() {
-        const CONTAINER_STYLES = [
-            'relative',
-            'flex flex-col',
-            'w-screen h-screen overflow-hidden',
-        ]
-
-        const { GRADIENTS } = useGradients({ hover: false })
-        const BG_STYLES = [
-            'bg-gradient-to-tr',
-            GRADIENTS.CAN_YOU_FEEL_THE_LOVE_TONIGHT,
-        ]
-
-        const containerStyles = computed(() => {
-            return [CONTAINER_STYLES, BG_STYLES]
-        })
-
-        const BODY_SECTION_STYLES = [
-            'absolute',
-            'w-full h-screen pt-12',
-            'snap-y overflow-y-scroll scroll-smooth box-content scroll-py-14',
-            'bg-black/25',
-        ]
-
-        const bodySectionStyles = computed(() => {
-            return [BODY_SECTION_STYLES]
-        })
-
         const { navDrawer } = useNavigationDrawer()
+
+        // Handle routing
+        const router = useRouter()
 
         // Handle data
         const { posts: postModule } = useStore()
-        const { homeFeed, fetchHomeFeedPosts, clearPostStorage } = postModule
+        const { homeFeed, fetchHomeFeedPosts } = postModule
 
-        // clearPostStorage()
-        fetchHomeFeedPosts()
-
-        console.log('homeFeed', homeFeed)
+        // Fetch posts from API
+        if (
+            !Boolean(homeFeed.value?.posts) ||
+            homeFeed.value?.posts.length == 0
+        ) {
+            fetchHomeFeedPosts()
+        }
 
         return {
             navDrawer,
-            containerStyles,
-            bodySectionStyles,
+            router,
             homeFeed,
         }
     },
-
-    mounted() {
-        console.log('mounted', this.homeFeed)
-    },
-
     methods: {
         getContentFromPostBody(postBody) {
             return {
                 ops: [{ insert: postBody }],
             }
+        },
+        goToPost(post) {
+            this.router.push(`/posts/${post.id}`)
         },
     },
 }
---
+++ app/src/pages/post.vue
@@ -0,0 +1,76 @@
+<template>
+    <dn-page
+        ref="page"
+        swipable
+        @toggle-navigation-drawer="navDrawer = !navDrawer"
+        @swipe-end="goToHomePage"
+    >
+        <!-- Post -->
+        <dn-card
+            class="snap-start mt-2"
+            :title="activePost.title"
+            :content="getContent(activePost.body)"
+        />
+
+        <!-- Comments -->
+        <dn-card
+            v-for="(comment, index) in activePostComments.comments"
+            :key="index"
+            class="snap-start mt-2"
+            dim
+            :content="getContent(comment.body)"
+        />
+    </dn-page>
+</template>
+
+<script>
+//-- Libraries
+import { useRoute } from 'vue-router'
+
+//-- Components
+import DnCard from '@cm/card.vue'
+import DnPage from '@ct/page.vue'
+
+//-- Store
+import useStore from '@/store/use-store'
+
+//-- Composables
+import useNavigationDrawer from '@/composables/use-navigation-drawer'
+
+export default {
+    name: 'dn-post',
+    components: { DnCard, DnPage },
+    setup() {
+        const { navDrawer } = useNavigationDrawer()
+
+        // Handle data
+        const { posts: postModule, comments: commentModule } = useStore()
+        const { activePost, setActivePost } = postModule
+        const { fetchComments, activePostComments } = commentModule
+
+        // Get active post id from route
+        const route = useRoute()
+        const postId = route.params.id
+
+        setActivePost({ postId })
+        fetchComments({ postId: activePost.value.id })
+
+        return {
+            navDrawer,
+            activePost,
+            activePostComments,
+        }
+    },
+
+    methods: {
+        getContent(body) {
+            return {
+                ops: [{ insert: body }],
+            }
+        },
+        goToHomePage() {
+            this.$router.push({ name: 'Home' })
+        },
+    },
+}
+</script>
